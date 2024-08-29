bc0a051616b09ae679c4422d06a29b7c6b739b75
Karambeium <kjivani07@gmail.com>
2024-08-29 19:33:58 +0000

    initial project structure

---
+++ springboot/stanley-jobs/src/test/java/com/stanley/stanley_jobs/StanleyJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stanley.stanley_jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanleyJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
